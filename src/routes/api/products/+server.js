76a4f7f4ce986ed9f150f6c84ed34f6447e8bf96
Lacey Pevey <7490308+pevey@users.noreply.github.com>
2023-06-01 10:08:18 -0500

    remove console.log

--- src/routes/api/products/+server.js
+++ src/routes/api/products/+server.js
@@ -2,7 +2,6 @@ import productService from '$lib/server/productService'
 import { error, json } from '@sveltejs/kit'
 
 export async function GET() {
-	console.log(productService)
 	let data = await productService.list()
 	return data? json(data) : error(400, 'No products found')
 }
