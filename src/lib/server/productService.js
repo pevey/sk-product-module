b559368c27cf1bd6489e6570a41fbe07463f2328
Lacey Pevey <7490308+pevey@users.noreply.github.com>
2023-06-01 09:51:56 -0500

    add db url to initialize options

--- src/lib/server/productService.js
+++ src/lib/server/productService.js
@@ -1,5 +1,9 @@
 import { initialize as ProductModuleInitialize } from '@medusajs/product'
 import { POSTGRES_URL } from '$env/static/private'
 
-const productService = await ProductModuleInitialize()
+const productService = await ProductModuleInitialize({
+	 database: {
+		clientUrl: POSTGRES_URL
+	 }
+})
 export default productService
